14b871fce41fa226dcf3faa699631e5aab3cef12
Artem Butusov <artem.butusov@unvsoft.com>
2016-09-06 23:23:46 -0400

    bootstrap override example

--- src/Hello.tsx
+++ src/Hello.tsx
@@ -7,6 +7,13 @@ export interface HelloProps {
 
 export class Hello extends React.Component<HelloProps, {}> {
   render() {
-    return <h1>Hello from {this.props.compiler} and {this.props.framework}</h1>;
+    return (
+      <div style={{ margin: '20px' }}>
+        <h1>Hello from {this.props.compiler} and {this.props.framework}</h1>
+        <button type="button" className="btn btn-primary">
+          This button should be red if bootstrap overrides work well
+        </button>
+      </div>
+    );
   }
 }
