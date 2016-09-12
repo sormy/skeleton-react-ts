4230e36db7bce37523fe4dbe3f1fd82a3a41828b
Artem Butusov <artem.butusov@unvsoft.com>
2016-09-12 08:45:27 -0400

    load bootstrap javascript library

--- src/boot.tsx
+++ src/boot.tsx
@@ -1,3 +1,5 @@
 import './bootstrap/bootstrap.less';
+import 'bootstrap/dist/js/bootstrap.js';
+
 import 'font-awesome/less/font-awesome.less';
 import 'open-sans-fontface/open-sans.less';
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import './styles';
+import './boot';
 
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
