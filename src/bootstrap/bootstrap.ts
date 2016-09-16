09f0e345207411b3010485667b0ce4e5acc5101a
Artem Butusov <artem.butusov@unvsoft.com>
2016-09-16 17:55:49 -0400

    revert bootstrap 3 import

--- src/bootstrap/bootstrap.ts
+++ src/bootstrap/bootstrap.ts
@@ -1,12 +1,15 @@
-import 'bootstrap/js/affix';
+/* tslint:disable:ordered-imports */
+
+// keep vanilla order modules
+import 'bootstrap/js/transition';
 import 'bootstrap/js/alert';
 import 'bootstrap/js/button';
 import 'bootstrap/js/carousel';
 import 'bootstrap/js/collapse';
 import 'bootstrap/js/dropdown';
 import 'bootstrap/js/modal';
+import 'bootstrap/js/tooltip';
 import 'bootstrap/js/popover';
 import 'bootstrap/js/scrollspy';
 import 'bootstrap/js/tab';
-import 'bootstrap/js/tooltip';
-import 'bootstrap/js/transition';
+import 'bootstrap/js/affix';
